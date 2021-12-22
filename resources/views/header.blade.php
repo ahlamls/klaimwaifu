e657c30a925f1eb2400e3a609e3973c4b8d6412d
ahlamls <fulldoang@gmail.com>
2021-12-23 00:52:56 +0700

    tambah meta tag mark

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -4,7 +4,10 @@
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
+    <meta property="og:type"               content="article" />
+<meta property="og:title"              content="Klaim Waifu/Husbando by himputek.id" />
+<meta property="og:description"        content="Situs untuk klaim waifu kalian! Social experiment untuk cek waifu paling banyak diklaim" />
+<meta property="og:image"              content="https://klaimwaifu.himputek.id/istri_revisi_fix_uw.png" />
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="icon" href="https://himputek.id/wp-content/uploads/2021/12/cropped-PicsArt_11-18-09.17.19-scaled-e1639578353229-192x192.jpg" sizes="192x192">
